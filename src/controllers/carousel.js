52abece9ca4c56dbbd9d2ba5ef911467a9ec0c5a
imronshoimov <imronshoimov125@gmail.com>
2021-10-18 17:33:23 -0700

    changes

--- src/controllers/carousel.js
+++ src/controllers/carousel.js
@@ -4,13 +4,17 @@ const fs = require("fs");
 const path = require("path");
 
 exports.getData = async (req, res) => {
-    const data = await model.getCarousel();
-    if(data) {
-        res.status(200)
-            .send(data);
-    } else {
-        res.status(401)
-            .json({ message: "There is an error, please try again!" });
+    try {
+        const data = await model.getCarousel();
+        if(data) {
+            res.status(200)
+                .send(data);
+        } else {
+            res.status(401)
+                .json({ message: "There is an error, please try again!" });
+        };
+    } catch (err) {
+        throw err
     };
 };
 
