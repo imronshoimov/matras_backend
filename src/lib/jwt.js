efab6fc61e5b28805a2c3f819c1c369309d4aa56
imronshoimov <imronshoimov125@gmail.com>
2021-10-20 11:11:07 -0700

    jwt change

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -2,6 +2,6 @@ const jwt = require("jsonwebtoken");
 const { secretKey } = require("../config/keys");
 
 module.exports = {
-    sign: payload => jwt.sign(payload, secretKey, { expiresIn: "24h" }),
+    sign: payload => jwt.sign(payload, secretKey, { expiresIn: "60h" }),
     verify: token => jwt.verify(token, secretKey)
 };
