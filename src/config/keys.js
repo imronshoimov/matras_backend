42789c61f10ae00aa755766574a50b7b861d22f9
imronshoimov <imronshoimov125@gmail.com>
2021-11-08 22:38:05 +0500

    changes

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT,
-    connectionString: process.env.DB_CONNNECT,
+    connectionString: process.env.DB_CONNNECT || "postgres://oucplina:2jdpz6RLB9na7J4PUinfmcQ2vq_ABO5E@fanny.db.elephantsql.com/oucplina",
     pageLimit: 2,
     secretKey: process.env.SECRET_KEY
 }
